885d2d75c1a1d80c2493415850d04f8a237a3f53
Maciej-Motyka <maciekmotyka7@gmail.com>
2019-11-04 21:13:44 +0100

    working calculate_split.py

--- calculate_split.py
+++ calculate_split.py
@@ -1,10 +1,26 @@
-# Calculate optimal split parameter for savage
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+import sys
+import pdb
+
+usage = """
+'Calculate optimal split parameter for savage'
+"""
 
 def main():
-    add_argument('--ref_len', dest='ref_len', type=int, help='length in bp of the sequenced genome to calculate --split parameter for savage')
-    add_argument('-s', dest='input_s', type=str, help='path to input fastq containing single-end reads')
-    add_argument('-p1', dest='input_p1', type=str, help='path to input fastq containing paired-end reads (/1)')
-    add_argument('-p2', dest='input_p2', type=str, help='path to input fastq containing paired-end reads (/2)')
+    parser = ArgumentParser(description = usage)
+    parser.add_argument('--ref_len', dest='ref_len', type=int, help='length in bp of the sequenced genome to calculate --split parameter for savage')
+    parser.add_argument('-s', dest='input_s', type=str, help='path to input fastq containing single-end reads')
+    parser.add_argument('-p1', dest='input_p1', type=str, help='path to input fastq containing paired-end reads (/1)')
+    parser.add_argument('-p2', dest='input_p2', type=str, help='path to input fastq containing paired-end reads (/2)')
+
+    if len(sys.argv[1:])==0:
+#        print usage
+#        parser.print_usage()
+        parser.print_help()
+        parser.exit()
+    args = parser.parse_args()
 
     # analyze single-end input reads
     if args.input_s:
@@ -17,16 +33,22 @@ def main():
         [p1_total_len] = analyze_fastq(args.input_p1)
         [p2_total_len] = analyze_fastq(args.input_p2)
         p_total_len = p1_total_len + p2_total_len
-        if p1_seq_count != p2_seq_count:
-            sys.stderr.write("""ERROR: Unequal number of /1 and /2 reads. Exiting.\n""")
-            sys.stderr.flush()
-            sys.exit(1)
 
     total_seq_len = s_total_len + p_total_len
-    if not total_seq_len > 0:
-        sys.stderr.write("""ERROR: Total input length is zero. Exiting.\n""")
-        sys.stderr.flush()
-        sys.exit(1)
+    assert total_seq_len > 0, "Total length of input sequences is zero."
+
+    split_into = calculate_split(total_seq_len, args.ref_len)
+    filename = args.input_p1
+
+    outfile = open("split_into.txt", "a+")
+    outfile.write("%s\t%s\n" % (filename, split_into))
+    outfile.close()
+
+def calculate_split(num_of_bases, ref_len):
+    split_into = 1
+    while (num_of_bases / ref_len / split_into > 1000):
+        split_into += 1
+    return split_into
 
 def analyze_fastq(filename):
     total_len = 0
@@ -37,7 +59,7 @@ def analyze_fastq(filename):
                 l = len(line.strip('\n'))
                 total_len += l
             i += 1
-        assert i % 4 == 0 # fastq
+        assert i % 4 == 0, "%s is not a valid fastq file" % filename
     return [total_len]
 
 if __name__ == '__main__':
